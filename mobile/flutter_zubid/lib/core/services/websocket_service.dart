54e729267a332b69de301c5f5f976fec9e44479f
dalyan <dalyan.00297540@gmail.com>
2026-01-07 14:18:45 +0300

    refactor: Remove unused imports in websocket_service.dart

--- mobile/flutter_zubid/lib/core/services/websocket_service.dart
+++ mobile/flutter_zubid/lib/core/services/websocket_service.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/status.dart' as status;
 
